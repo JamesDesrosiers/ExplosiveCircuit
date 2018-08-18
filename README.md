f580053107686968243237220c1512e928e10505
JamesDesRosiers <JamesDavidDesrosiers@gmail.com>
2018-08-18 18:09:05 -0400

    Added menu

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ExplosiveCircuit
+
+Contains Spider Talent Font, by Chequered Ink under License for Non-commercial and personal use.
---
+++ _Scenes/TitleScreen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_Startbutton_pressed():
+	$ColorRect.visible = true
+	$Tween.interpolate_property($"ColorRect", "color", Color(0,0,0,0), Color(0,0,0,1), 1, Tween.TRANS_QUAD, Tween.EASE_IN)
+	$Tween.start()
+	yield($Tween, "tween_completed")
+	get_tree().change_scene("res://_Scenes/base.tscn")
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
