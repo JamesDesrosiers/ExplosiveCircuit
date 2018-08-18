d3c684b48e5bc111fc640166bedad366e5e1ba11
JamesDesRosiers <JamesDavidDesrosiers@gmail.com>
2018-08-18 13:38:00 -0400

    Rigged the Force High function on control nodes.

--- _Scenes/ControlPoint.gd
+++ _Scenes/ControlPoint.gd
@@ -9,9 +9,9 @@ var leftConnect
 var rightConnect
 
 var volt = 0
-var state = State.free
+var state = STATE.free
 
-enum State{
+enum STATE{
 	free,
 	high,
 	low
@@ -25,28 +25,24 @@ func _process(delta):
 	update_volt()
 
 func is_source():
-	return state == State.high
+	return state == STATE.high
 
 func update_volt():
-	
-	var volt_left
-	var volt_right
-	
-	if(leftConnect != null and leftConnect.has_method("get_volt")):
-		volt_left = leftConnect.get_volt()
-	else: volt_left = 0
-	if(rightConnect != null and rightConnect.has_method("get_volt")):
-		volt_right = rightConnect.get_volt()
-	else: volt_right = 0
-	
-	if(state == State.free):
-		if(volt_right > 120 or volt_left > 120):
-			self.queue_free()
-		elif(volt_left > volt_right):
+	if(state == STATE.free):	
+		var volt_left
+		var volt_right
+		
+		if(leftConnect != null and leftConnect.has_method("get_volt")):
+			volt_left = leftConnect.get_volt()
+		else: volt_left = 0
+		if(rightConnect != null and rightConnect.has_method("get_volt")):
+			volt_right = rightConnect.get_volt()
+		else: volt_right = 0
+		if(volt_left > volt_right):
 			volt = volt_left
 		else:
 			volt = volt_right
-	elif(state == State.high):
+	elif(state == STATE.high):
 		volt = HIGH
 	else:
 		volt = 0
@@ -64,13 +60,18 @@ func _on_Right_area_entered(area):
 func _on_TextureButton_pressed():
 	if(menu != null): return
 	
-	menu = MENU.instance()
-	menu.connect("_on_destroy", self, "_menu_callback")
+	menu = MENU.instance(state)
+	menu.connect("_on_destroy", self, "_menu_gone")
+	menu.connect("_on_high", self, "set_high")
 	get_node("Canvas").add_child(menu)
 	get_node("Canvas").offset = Vector2(position.x-40, position.y-40)
-	print(menu.rect_position)
 	
+	menu.update(state)
 	menu.open()
 
-func _menu_callback():
+func _menu_gone():
 	menu = null
+
+func set_high():
+	state = STATE.high
+	menu.update(state)
--- _Scenes/RadialMenu.gd
+++ _Scenes/RadialMenu.gd
@@ -2,9 +2,21 @@ extends Control
 
 var popup
 signal _on_destroy
+signal _on_high
+
+enum STATE{
+	free,
+	high,
+	low
+	}
 
 func _ready():
 	popup = get_node("CenterContainer/Popup")
+
+func update(state):
+	if(state == STATE.high):
+		get_node("CenterContainer/Popup/HighButton").disabled = true
+	return
 
 func _on_BackButton_pressed():
 	emit_signal("_on_destroy")
@@ -18,3 +30,6 @@ func empty_margin():
 	margin_right = 0
 	margin_top = 0
 	margin_bottom = 0
+
+func _on_HighButton_pressed():
+	emit_signal("_on_high")
